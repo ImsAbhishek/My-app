41d0ce83d8d102dca3b785547f623e6c711d032b
ABHISHEK SINGH <15singhabhishekrajput@gmail.com>
2022-05-04 06:26:27 +0000

    script.sh edited online with Bitbucket

--- script.sh
+++ script.sh
@@ -5,11 +5,12 @@ bn=$2
 GIT=`which git`
 # REPO_DIR=/
 # cd ${REPO_DIR}
-${GIT} init
+
 ss=$(${GIT} ls-remote --heads https://ImsAbhishek:${key}@github.com/ImsAbhishek/My-app.git $branch | wc -l)
 if [[ "$ss" == 0 ]];
 then
  echo "branch does't exist"
+ ${GIT} init
  ${GIT} checkout -b $branch
 else
     echo "branch exist"
